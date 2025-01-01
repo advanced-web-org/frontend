e366ad62f1f6332cfd49b52b489e580723a2f555
GiaThinn <trangiathinh53@gmail.com>
2025-01-01 20:36:41 +0700

    fix dashboard ui

--- src/pages/customer/pages/dashboard.tsx
+++ src/pages/customer/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Eye, EyeOff, Send, History } from "lucide-react";
+import { Eye, EyeOff, Send, History, Landmark, Banknote, BadgeDollarSign } from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { useUserStore } from "@/stores/userStore";
 import { Button } from "@/components/ui/button";
@@ -13,36 +13,56 @@ export default function DashboardPage() {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <Card className="max-w-md mx-auto p-4 flex flex-col gap-3 justify-between">
-        <div>
-          <div className="flex justify-between items-center">
-            <span className="font-semibold">
-              {user?.account_number?.replace(/(\d{4})(\d{4})(\d{3})/, "$1 $2 $3")}
-            </span>
+    <div className="container mx-auto p-2 flex flex-col justify-start gap-10">
+      {/* Header */}
+      <div className="">
+        <h1 className="text-2xl font-semibold">Welcome, {user?.fullname || 'Customer'}</h1>
+      </div>
+      {/* Account */}
+      <div className="flex flex-col gap-4">
+        <h2 className="text-base font-medium">List accounts</h2>
+        <Card className="max-w-sm p-4 flex flex-col gap-3 justify-between">
+          <div>
+            <div className="flex items-center gap-4">
+              <Landmark size={20} color="#81dbe3"/>
+              <span className="font-medium">
+                {user?.account_number?.replace(/(\d{4})(\d{4})(\d{3})/, "$1 $2 $3")}
+              </span>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="flex justify-between items-center w-3/5 mt-2">
-            <span className="flex items-center font-semibold text-2xl">
-              {isBalanceHidden ? "******** VNĐ" : `${user?.account_balance?.toLocaleString('vi-VN')} VNĐ`}
-            </span>
-            <Button variant="ghost" className="ml-2" onClick={toggleBalanceVisibility}>
-              {isBalanceHidden ? <EyeOff size={22} /> : <Eye size={20} />}
-            </Button>
+          <div>
+            <div className="flex items-center mt-2 gap-3 w-full justify-between">
+              <div className="flex items-center gap-4">
+                <Banknote size={20} color="#81dbe3" />
+                <span className="flex items-center font-semibold text-2xl">
+                  {isBalanceHidden ? "******** VNĐ" : `${user?.account_balance?.toLocaleString('vi-VN')} VNĐ`}
+                </span>
+              </div>
+              <Button variant="ghost" className="ml-2" onClick={toggleBalanceVisibility}>
+                {isBalanceHidden ? <EyeOff size={22} color="#81dbe3" /> : <Eye size={20} color="#81dbe3" />}
+              </Button>
+            </div>
           </div>
+        </Card>
+      </div>
+      {/* Services */}
+      <div className="flex flex-col gap-4">
+        <h2 className="text-base font-medium">Services</h2>
+        <div className="flex gap-10">
+          <Card className="flex flex-col gap-2 p-4 items-center justify-center w-40 h-32">
+            <Send size={28} color="#573681" />
+            <span className="text-base font-medium">Internal Transfer</span>
+          </Card>
+          <Card className="flex flex-col gap-2 p-4 items-center justify-center w-40 h-32">
+            <BadgeDollarSign size={28} color="#eddc83" />
+            <span className="text-base font-medium">External Transfer</span>
+          </Card>
+          <Card className="flex flex-col gap-2 p-4 items-center justify-center w-40 h-32">
+            <History size={28} color="#f69039" />
+            <span className="text-base font-medium">History</span>
+          </Card>
         </div>
-        <div className="flex justify-around items-center mt-4">
-          <Button variant="ghost" className="flex flex-col space-y-0 gap-1">
-            <Send size={18} />
-            <span className="mt-1 text-xs">Transfer</span>
-          </Button>
-          <Button variant="ghost" className="flex flex-col space-y-0 gap-1">
-            <History size={18} />
-            <span className="mt-1 text-xs">TRXN history</span>
-          </Button>
-        </div>
-      </Card>
+      </div>
     </div>
   );
 }
