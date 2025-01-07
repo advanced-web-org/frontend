58fd1858abde3e2e191833d30ece669674a071b8
GiaThinn <trangiathinh53@gmail.com>
2025-01-07 21:51:33 +0700

    show beneficiary table

--- src/pages/customer/pages/Beneficienary/beneficiary.tsx
+++ src/pages/customer/pages/Beneficienary/beneficiary.tsx
@@ -1,7 +1,12 @@
 import { Beneficiary, getBeneficiaries } from "@/api/beneficiaries/beneficiary";
-import { DataTable } from "../../components/tables/table";
-import { columns } from "./columns";
 import { useEffect, useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function BeneficiaryPage() {
   const [beneficiaries, setBeneficiaries] = useState<Beneficiary[]>([]);
@@ -15,9 +20,39 @@ export default function BeneficiaryPage() {
   }, []);
 
   return (
-    <div>
-      <p className="text-2xl pt-1 pb-3 font-semibold">Beneficiaries</p>
-      <DataTable columns={columns} data={beneficiaries} />
+    <div className="container mx-auto p-2 flex flex-col justify-start gap-10">
+      {/* Header */}
+      <div className="">
+        <h1 className="text-2xl font-semibold">Beneficiaries</h1>
+      </div>
+      {/* Table */}
+      <div className="px-10">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell><span className="font-semibold">Bank name</span></TableCell>
+                <TableCell><span className="font-semibold">Account Number</span></TableCell>
+                <TableCell><span className="font-semibold">Nickname</span></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {beneficiaries.map((beneficiary) => (
+                <TableRow
+                  key={beneficiary.beneficiary_id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {beneficiary.bank_name}
+                  </TableCell>
+                  <TableCell>{beneficiary.account_number}</TableCell>
+                  <TableCell>{beneficiary.nickname}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 }
